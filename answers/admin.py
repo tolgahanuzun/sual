05582f36624c007ac06938e94e8e38ac4c95a0f9
tolgahanuzun <mail@tolgahanuzun.com>
2017-10-30 10:27:41 +0300

    first code

--- answers/admin.py
+++ answers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from answers.models import Answers
 # Register your models here.
+
+admin.site.register(Answers)
--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+"Answers Model"
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from questions.models import Questions
+
+class Answers(models.Model):
+    "Answers Model"
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+    body = models.TextField(max_length=300, blank=True, verbose_name="Company")
+
+    def __str__(self):
+        return "%s" % (self.question)
